2c54528d4fb5de04a81cbcecd7d491a23639c01a
yaokun <yaokun@baidu.com>
2016-02-11 23:52:57 +0800

    add

--- textcode/ui/admin/index.php
+++ textcode/ui/admin/index.php
@@ -1,11 +1,11 @@
 <?php
 
 class index {
+    const TPL = "setting";
     
     const DB_NAME = "test";
 
     public function execute() {
-        echo Lib_Const::TEST;
         $db = Storage_Mongo::getDb(self::DB_NAME);
         $res = $db->textcode->findOne();
         var_dump($res);
