a9d6a03e29e4a49e03ab2c34b400886688d1da6d
yaokun <yaokun@baidu.com>
2016-02-11 23:47:20 +0800

    add

--- textcode/ui/admin/index.php
+++ textcode/ui/admin/index.php
@@ -1,11 +1,13 @@
 <?php
 
 class index {
-    const TPL = "setting";
     
+    const DB_NAME = "test";
+
     public function execute() {
-        $title = Http_Request::get('title', 0);
-        $nav1 = Http_Request::get('nav1', '');
+        $db = Storage_Mongo::getDb(self::DB_NAME);
+        $res = $db->textcode->findOne();
+        var_dump($res);
         Page::setTpl(self::TPL);
     }
 }
