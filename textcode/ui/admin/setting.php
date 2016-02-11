cdcbee5f7bacc1d857d23b8b0711b84765f66d14
yaokun <yaokun@baidu.com>
2016-02-11 23:45:45 +0800

    add

--- textcode/ui/admin/setting.php
+++ textcode/ui/admin/setting.php
@@ -1,13 +1,19 @@
 <?php
 
 class setting {
+    const DB_NAME = "test";
 
     public static function execute() {
-        $title = Http_Request::get('title', 0);
+        $title = Http_Request::get('title', '');
         $nav1 = Http_Request::get('nav1', '');
-        $arrRet = array(
-            'yaokun' => 123,
+        $db = Storage_Mongo::getDb(self::DB_NAME);
+        $doc = array(
+            'title' => $title,
+            'nav1' => $nav1,
         );
+        $res = $db->textcode->insert($doc);
+        $arrRet = array();
+        $arrRet = $res;
         Page::jsonRet(0, 'success', $arrRet);
         return true;
     }
