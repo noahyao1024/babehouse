de026f44b84f124e5b657ad2139040b9c9bcb796
syaokun219 <627571@qq.com>
2016-11-15 09:35:50 +0800

    fixed codestyle

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ function init() {
     $request_uri = $_SERVER['REQUEST_URI'];
     $arr_tmp = explode("/", $request_uri);
     $app_name = $arr_tmp[2];
-    define(APP_NAME, $app_name);
+    define("APP_NAME", $app_name);
 }
 
 try {
